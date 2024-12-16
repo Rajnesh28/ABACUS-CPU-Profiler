75c3cacab043856a3db60ce9749e66c906150599
Rajnesh Joshi <rajnesh.joshi28@gmail.com>
2024-12-15 18:29:24 -0800

    Add AXI-Lite bus, and modify cache profiler to track rising edges of certain CPU signals

--- HDL/profiling_units/cache_profiler.sv
+++ HDL/profiling_units/cache_profiler.sv
@@ -46,81 +46,31 @@ state_t dcache_hit_state, dcache_request_state;
 
 
 always_ff @(posedge clk) begin
-    if (rst | ~enable) begin
+    if (rst) begin
+        icache_request_counter_reg <= 32'h0;
+        icache_miss_counter_reg <= 32'h0;
         icache_hit_counter_reg <= 32'h0;
         dcache_miss_counter_reg <= 32'h0;
-    end else begin
-        icache_hit_counter_reg <= icache_request_counter_reg - icache_miss_counter_reg;
-        dcache_miss_counter_reg <= dcache_request_counter_reg - dcache_hit_counter_reg;
-    end
-end
+        dcache_hit_counter_reg <= 32'h0;
+        dcache_line_fill_latency_counter_reg <= 32'h0;
 
-always_ff @(posedge clk) begin
-    if (rst || ~enable) begin
-        icache_miss_state <= IDLE;    
-        icache_miss_counter_reg <= 32'h0;    
-    end else begin
-        case (icache_miss_state)
-            IDLE: begin
-                if (icache_miss) begin
-                    icache_miss_counter_reg <= icache_miss_counter_reg + 1;
-                    icache_miss_state <= WAIT_LOW;
-                end
-            end
-            WAIT_LOW: begin
-                if (!icache_miss) begin
-                    icache_miss_state <= IDLE;
-                end
-            end
-        endcase
-    end
-end
+        icache_request_prev <= 1'b0;
+        dcache_request_prev <= 1'b0;
+        icache_miss_prev <= 1'b0;
+        dcache_hit_prev <= 1'b0;
+        dcache_line_fill_in_progress_prev <= 1'b0;
 
-always_ff @(posedge clk) begin
-    if (rst | ~enable) begin
-        icache_request_state <= IDLE;
-        icache_request_counter_reg <= 32'h0;    
-    end else begin
-        case (icache_request_state)
-            IDLE: begin
-                if (icache_request) begin
-                    icache_request_counter_reg <= icache_request_counter_reg + 1;
-                    icache_request_state <= WAIT_LOW;
-                end
-            end
-            WAIT_LOW: begin
-                if (!icache_request) begin
-                    icache_request_state <= IDLE;
-                end
-            end
-        endcase
-    end
-end
+    end else if (~enable) begin
+        icache_request_counter_reg <= 32'h0;
+        icache_miss_counter_reg <= 32'h0;
+        icache_hit_counter_reg <= 32'h0;
+        icache_line_fill_latency_counter_reg <= 32'h0;
 
-always_ff @(posedge clk) begin
-    if (rst | ~enable) begin
-        dcache_hit_state <= RESET;
-    end else if (~rst & enable) begin
-        case (dcache_hit_state)
-            IDLE: begin
-                if (dcache_hit) begin
-                    dcache_hit_counter_reg <= dcache_hit_counter_reg + 1;
-                    dcache_hit_state <= WAIT_LOW;
-                end
-            end
-            WAIT_LOW: begin
-                if (!dcache_hit) begin
-                    dcache_hit_state <= IDLE;
-                end
-            end
-        endcase
-    end
-end
-
-always_ff @(posedge clk) begin
-    if (rst | ~enable) begin
-        dcache_request_state <= IDLE;
         dcache_request_counter_reg <= 32'h0;
+        dcache_miss_counter_reg <= 32'h0;
+        dcache_hit_counter_reg <= 32'h0;
+        dcache_line_fill_latency_counter_reg <= 32'h0;
+
     end else begin
         case (dcache_request_state)
             IDLE: begin
@@ -155,19 +105,40 @@ always_ff @(posedge clk) begin
         if (dcache_line_fill_in_progress) begin
             dcache_line_fill_latency_counter_reg <= dcache_line_fill_latency_counter_reg + 1;
         end
+
+        if (~dcache_line_fill_in_progress_prev & dcache_line_fill_in_progress) begin
+            dcache_miss_counter_reg <= dcache_miss_counter_reg + 1;
+        end
+        dcache_line_fill_in_progress_prev <= dcache_line_fill_in_progress;
+
+        icache_hit_counter_reg <= icache_request_counter_reg - icache_miss_counter_reg;
+
     end
 end
 
-always_comb begin
-    icache_request_counter <= icache_request_counter_reg;
-    icache_hit_counter <= icache_request_counter_reg - icache_miss_counter_reg;
-    icache_miss_counter <= icache_miss_counter_reg;
-    icache_line_fill_latency_counter <= icache_line_fill_latency_counter_reg;
 
-    dcache_request_counter <= dcache_request_counter_reg;
-    dcache_hit_counter <= dcache_hit_counter_reg;
-    dcache_miss_counter <= dcache_request_counter_reg - dcache_hit_counter_reg;
-    dcache_line_fill_latency_counter <= dcache_line_fill_latency_counter_reg;
+always_ff @(posedge clk)  begin
+    if (rst) begin
+        i <= 0;
+    end else begin
+        if (~enable) begin
+            i <= 0;
+        end
+        if (i == CLOCK_FREQ*2) begin  // Update all registers every two seconds for data consistency
+            icache_request_counter <= icache_request_counter_reg;
+            icache_hit_counter <= icache_hit_counter_reg;
+            icache_miss_counter <= icache_miss_counter_reg;
+            icache_line_fill_latency_counter <= icache_line_fill_latency_counter_reg;
+
+            dcache_request_counter <= dcache_request_counter_reg;
+            dcache_hit_counter <= dcache_hit_counter_reg;
+            dcache_miss_counter <= dcache_miss_counter_reg;
+            dcache_line_fill_latency_counter <= dcache_line_fill_latency_counter_reg;
+            i <= 0;
+        end else begin
+            i <= i + 1;
+        end
+    end
 end
 
 endmodule
